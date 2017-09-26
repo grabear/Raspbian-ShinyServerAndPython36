a0970c302b1bf0a2d01b7460f7e41c955bf81a6f
rgilmore <rgilmore@umc.edu>
2017-09-26 17:55:55 -0500

    Added -y to apt-get commands for auto answering prompts during installation.

--- config/python-3.6/py36-install.sh
+++ config/python-3.6/py36-install.sh
@@ -7,7 +7,7 @@
 
 # Install the required build-tools (some might already be installed on your system).
 sudo apt-get update
-sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+sudo apt-get -y install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 
 # Download and install Python 3.6
 wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tar.xz
@@ -20,10 +20,10 @@ sudo make altinstall
 # Delete the source code and uninstall the previously installed packages. When uninstalling the packages, make sure you only remove those that were not previously installed on your system
 sudo rm -r Python-3.6.2
 sudo mv Python-3.6.2.tgz ~/Downloads/
-sudo apt-get --purge remove build-essential tk-dev
-sudo apt-get --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
-sudo apt-get --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
-sudo apt-get --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-sudo apt-get autoremove
-sudo apt-get clean
+sudo apt-get -y --purge remove build-essential tk-dev
+sudo apt-get -y --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
+sudo apt-get -y --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
+sudo apt-get -y --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+sudo apt-get -y autoremove
+sudo apt-get -y clean
 
--- config/shiny-server/shiny-server-install.sh
+++ config/shiny-server/shiny-server-install.sh
@@ -14,7 +14,7 @@ sudo echo "deb http://http://mirrors.nics.utk.edu/cran/bin/linux/debian stretch-
 sudo cat /etc/apt/sources.list
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
 sudo apt-get update
-sudo apt-get install r-base r-base-dev
+sudo apt-get -y install r-base r-base-dev
 
 # Install R packages
 Rscript requirements.R
