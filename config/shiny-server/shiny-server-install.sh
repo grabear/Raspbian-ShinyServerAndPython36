cce9879e167e67844e61e521b468b8210926c342
rgilmore <rgilmore@umc.edu>
2017-09-26 16:12:23 -0500

    Added bash commands to install R.

--- config/shiny-server/shiny-server-install.sh
+++ config/shiny-server/shiny-server-install.sh
@@ -9,6 +9,14 @@ cd cmake-2.8.11.2
 make
 sudo make install
 
+# Install R
+sudo echo "deb http://http://mirrors.nics.utk.edu/cran/bin/linux/debian stretch-cran34/" >> /etc/apt/sources.list
+sudo cat /etc/apt/sources.list
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
+sudo apt-get update
+sudo apt-get install r-base r-base-dev
+
+sudo apt-key add --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 # Clone the repository from GitHub
 git clone https://github.com/rstudio/shiny-server.git
 
