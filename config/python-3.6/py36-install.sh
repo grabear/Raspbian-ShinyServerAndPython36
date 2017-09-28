b7bb5af09adfec70ff4e433cdc5b1f832094c8e3
rgilmore <rgilmore@umc.edu>
2017-09-28 15:35:16 -0500

    Removed apt-dependencies.  Removed purge of necessary apt packages.

--- config/python-3.6/py36-install.sh
+++ config/python-3.6/py36-install.sh
@@ -5,10 +5,6 @@
 ####
 ####
 
-# Install the required build-tools (some might already be installed on your system).
-sudo apt-get update
-sudo apt-get -y install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-
 # Download and install Python 3.6
 wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tar.xz
 tar xf Python-3.6.2.tar.xz
@@ -22,7 +18,7 @@ sudo rm -r Python-3.6.2
 sudo mv Python-3.6.2.tgz ~/Downloads/
 sudo apt-get -y --purge remove build-essential tk-dev
 sudo apt-get -y --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
-sudo apt-get -y --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
+sudo apt-get -y --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev
 sudo apt-get -y --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 sudo apt-get -y autoremove
 sudo apt-get -y clean
