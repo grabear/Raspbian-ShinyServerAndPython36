72c5ab3072dde0053392ab8f2f5ca7c9aba9eefd
rgilmore <rgilmore@umc.edu>
2017-09-28 15:02:27 -0500

    Cleaned up files and added a directory for R.

---
+++ config/R/requirements.R
@@ -0,0 +1,11 @@
+# CRAN packages
+install.packages(c('devtools', 'shiny', 'shinythemes', 'shinyBS', 'shinyjs', 'europepmc', 'DT', 'data.table', 'markdown', 'rclipboard', 'shinycssloaders', 'shinydashboard', 'shinyFiles', 'shinyFeedback','shinyHeatmaply', 'shinyjqui', 'shinyWidgets', 'bsplus', 'colourpicker'), repos="https://mirrors.nics.utk.edu/cran")
+
+# GitHub Packages
+library(devtools)
+install_github(c("cstubben/euPMC", "robear22890/shinyLP", "mkuhn/dict", 'ayayron/shinydnd', "Appsilon/shiny.semantic", "rstudio/miniUI", 'zachcp/msaR'))
+
+# Bioconductor packages
+source("https://bioconductor.org/biocLite.R")
+biocLite("ggtree")
+biocLite("cummeRbund")
