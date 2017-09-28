854da430d4cd2df12f332c34e390b3e972ffdf17
rgilmore <rgilmore@umc.edu>
2017-09-28 15:55:51 -0500

    Added a master installation script.

---
+++ master-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# These must be installed before Python 3.6 and/or R can be installed
+# Install apt packages
+. config/apt/apt-install.sh
+
+# This must be installed before shiny-server is installed
+# Install R and R Packages
+. config/R/R-install.sh
+
+# Install shiny-server and enable autostart
+. config/shiny-server/shiny-server-install.sh
+. config/shiny-server/shiny-server-post-install.sh
+
+# Install python 3.6 and python packages
+. config/python-3.6/py36-install.sh
+
+sudo reboot
