fcb920a7b3dd5b2c2d8b15b1ef30793816282670
rgilmore <rgilmore@umc.edu>
2017-09-28 15:34:46 -0500

    Removed apt dependencies.  Cleaned.

--- config/R/R-install.sh
+++ config/R/R-install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-# Install Dependencies
-sudo apt-get install libxml2-dev libssl-dev libcurl4-openssl-dev libssh2-1-dev
+# Update the system
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
 # Install R
 sudo echo "deb http://http://mirrors.nics.utk.edu/cran/bin/linux/debian stretch-cran34/" >> /etc/apt/sources.list
 sudo cat /etc/apt/sources.list
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
-sudo apt-get update
-sudo apt-get install r-base r-base-dev
+sudo apt-get -y update
+sudo apt-get -y install r-base r-base-dev
+
+# Update the system
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 # Install R packages
 Rscript requirements.R
