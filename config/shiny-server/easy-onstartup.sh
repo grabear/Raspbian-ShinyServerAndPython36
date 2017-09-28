6a0bddecab4e4deaa16e0fd600b1b5388cc31641
rgilmore <rgilmore@umc.edu>
2017-09-28 17:06:18 -0500

    Deletes.

--- config/shiny-server/easy-onstartup.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo wget https://raw.github.com/rstudio/shiny-server/master/config/upstart/shiny-server.conf -O /etc/init/shiny-server.conf
--- python3.6.0-install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Install python 3.6.0
-. config/python-3.6/py36-apt-requirements.sh
-. config/python-3.6/py36-install.sh
