4e830b82c1523d3ca1c350668b250cb7f820aea0
rgilmore <rgilmore@umc.edu>
2017-09-28 17:04:25 -0500

    Added a master installation package.

---
+++ config/R/R-apt-requirements.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Update the system
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# Install R dependencies
+sudo apt-get -y install libxml2-dev libssl-dev libcurl4-openssl-dev libssh2-1-dev
+
+# Update the system
+sudo apt-get -y update
+sudo apt-get -y upgrade
